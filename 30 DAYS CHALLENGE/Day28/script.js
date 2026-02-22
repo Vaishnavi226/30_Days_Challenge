b65c0aaa2880e9ba3e61be3aef869138084bf547
Vaishnavi226 <vaishnavitripathi226@gmail.com>
2026-02-22 23:48:50 +0530

    initial commit

---
+++ 30 DAYS CHALLENGE/Day28/script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll(".btn-book");
+
+buttons.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    alert("Booking feature coming soon!");
+  });
+});
